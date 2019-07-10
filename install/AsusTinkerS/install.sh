4fd795b2411d248958c062ca52af0c585ce8c95f
Peter Jensen <peterbjergby@gmail.com>
2019-07-10 20:26:57 +0200

    more to install script

--- install/AsusTinkerS/install.sh
+++ install/AsusTinkerS/install.sh
@@ -9,5 +9,18 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF
 sudo apt-get update
 sudo apt-get install ros-melodic-ros-base
 
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+cd RobotLawnmover/ROS
+catkin_make
+
+echo "source ~/RobotLawnmover/ROS/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
 
 
