0a5ec477b7ad4102720f8ab2708f6330ddccb1a6
Peter <peterbjergby@gmail.com>
2019-07-14 11:11:18 +0000

    i2c-tools added to install script

--- install/AsusTinkerS/install.sh
+++ install/AsusTinkerS/install.sh
@@ -8,7 +8,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get install ros-melodic-ros-base
+sudo apt-get install ros-melodic-ros-base -Y
 
 sudo rosdep init
 rosdep update
@@ -16,7 +16,7 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # Install dependencies
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-dev python-pip -Y
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-dev python-pip i2c-tools -Y
 
 # install GPIO
 git clone http://github.com/TinkerBoard/gpio_lib_python --depth 1 GPIO_API_for_Python
